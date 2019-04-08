9c03b18ac0f936d32d274c02e10612f1a4222a78
Tech-A08 <tech-a08@macbook-air-3.lan>
2019-04-08 21:55:23 +0100

    added bmi calculator

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-var calculatorType = prompt("Which calculator do you desire? basic(b) or advance(a)");
-var firstNumber = prompt("Please choose the first number");
+var calculatorType = prompt("Which calculator do you desire? basic(b), advance(a) or BMI (bmi)");
+if(calculatorType != "bmi"){
+  var firstNumber = prompt("Please choose the first number");
+}
 
 if (calculatorType == "b"){
   var operation = prompt("Choose which operation to use, addidition(+), subtraction(-), multiplication(*) or division(/)");
-} else {
+} else if (calculatorType == "a") {
   var operation = prompt("Choose which operation to use, power(pow) or square root(sqrt)");
+} else if (calculatorType == "bmi"){
+  var measurementSystem = prompt("choose metric(m) or imperial(i) system")
 }
-if (operation != "sqrt"){
+
+if (operation == "a" || operation == "b"){
   var secondNumber = prompt("please choose a second number");
 }
 
@@ -24,4 +29,16 @@ if (operation == "pow" && calculatorType == "a"){
   alert(Number(firstNumber) ** Number(secondNumber));
 } else if (operation == "sqrt" && calculatorType == "a"){
   alert(Math.sqrt(Number(firstNumber)));
+}
+
+if (measurementSystem == "m"){
+  var weight = prompt("Please enter Weight in kilograms");
+  var height = prompt("Please enter Height in metres");
+  alert(Number(weight) / (Number(height) ** 2));
+}
+
+if (measurementSystem == "i"){
+  var weight = prompt("Please enter Weight in Pounds");
+  var height = prompt("Please enter Height in inches");
+  alert((Number(weight) / (Number(height) ** 2)) * 703);
 }
